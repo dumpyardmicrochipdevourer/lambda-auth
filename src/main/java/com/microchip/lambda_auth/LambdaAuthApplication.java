2a87ca77648e8f6a0e86d2bbda4303a593d197eb
anton <russiansoveet@gmail.com>
2026-09-24 13:11:22 +0300

    [init] initialize project

---
+++ src/main/java/com/microchip/lambda_auth/LambdaAuthApplication.java
@@ -0,0 +1,12 @@
+package com.microchip.lambda_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LambdaAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LambdaAuthApplication.class, args);
+    }
+}
---
+++ src/test/java/com/microchip/lambda_auth/LambdaAuthApplicationTests.java
@@ -0,0 +1,21 @@
+package com.microchip.lambda_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.postgresql.PostgreSQLContainer;
+
+@SpringBootTest
+@Testcontainers
+class LambdaAuthApplicationTests {
+
+    @Container
+    @ServiceConnection
+    static final PostgreSQLContainer POSTGRES = new PostgreSQLContainer("postgres:17-alpine");
+
+    @Test
+    void contextLoads() {
+    }
+}
